6d4a7e189b221e7f644d784bda33e53df9a40f99
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-04-12 20:11:20 +0200

    Applied my preferences

--- src/main/java/guru/springframework/jdbc/dao/BookDao.java
+++ src/main/java/guru/springframework/jdbc/dao/BookDao.java
@@ -2,13 +2,11 @@ package guru.springframework.jdbc.dao;
 
 import guru.springframework.jdbc.domain.Book;
 
-import java.util.List;
-
 /**
  * Created by jt on 8/29/21.
  */
 public interface BookDao {
-    
+
     Book getById(Long id);
 
     Book findBookByTitle(String title);
--- src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
+++ src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    Optional<Book> findBookByTitle(String title);
+    Optional<Book> findByTitle(String title);
 }
