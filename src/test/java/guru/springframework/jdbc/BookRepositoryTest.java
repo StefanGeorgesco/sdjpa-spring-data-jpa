1a4e9a4c7899324b4e8931e29caf9a4f165c3af6
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-04-13 18:49:22 +0200

    Terminated branch

---
+++ src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
@@ -0,0 +1,54 @@
+package guru.springframework.jdbc;
+
+import guru.springframework.jdbc.repositories.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Created by jt on 11/21/21.
+ */
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"guru.springframework.jdbc.dao"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookRepositoryTest {
+
+    @Autowired
+    BookRepository bookRepository;
+
+//    @Test
+//    void testBookStream() {
+//        AtomicInteger count = new AtomicInteger();
+//
+//        bookRepository.findAllByTitleNotNull().forEach(book -> {
+//            count.incrementAndGet();
+//        });
+//
+//        assertThat(count.get()).isGreaterThan(5);
+//    }
+
+    @Test
+    void testEmptyResultException() {
+
+        assertThrows(EmptyResultDataAccessException.class, () -> bookRepository.readByTitle("foobar4"));
+    }
+
+    @Test
+    void testNullParam() {
+        assertNull(bookRepository.getByTitle(null));
+    }
+
+    @Test
+    void testNoException() {
+
+        assertNull(bookRepository.getByTitle("foo"));
+    }
+}
