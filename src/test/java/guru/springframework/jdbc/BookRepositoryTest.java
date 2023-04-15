6c68c84990a9011d8d7232376b429f5c5b06c490
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-04-15 12:48:52 +0200

    Removed unnecessary @ComponentScan annotation

--- src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
+++ src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -22,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @ActiveProfiles("local")
 @DataJpaTest
-@ComponentScan(basePackages = {"guru.springframework.jdbc.dao"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BookRepositoryTest {
 
